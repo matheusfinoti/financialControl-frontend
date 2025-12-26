45767903fb783ab155261b3b11f413680ec4dfb1
matheusfinoti <matheusfinoti@gmail.com>
2025-12-26 14:52:24 +0100

    excluido component home e transferido dados para pages home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { CategoryListComponent } from './components/category-list/category-list';
-import { Home } from './components/home/home';
+import { Home } from './pages/home/home';
 import { TransactionEditComponent } from './components/transaction-edit/transaction-edit';
 
 export const routes: Routes = [
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { CategoryListComponent } from '../../components/category-list/category-list';
+import { CommonModule } from '@angular/common';
+import { TransactionListComponent } from '../../components/transaction-list/transaction-list';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [ CommonModule, TransactionListComponent],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
